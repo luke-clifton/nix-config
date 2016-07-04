e0ee939900b05acbb9cc3e653c1195b3314b7a7e
Luke Clifton <ltclifton@gmail.com>
2016-07-04 18:42:23 +1000

    surf

--- config.nix
+++ config.nix
@@ -1,12 +1,65 @@
 {
- packageOverrides = pkgs: rec {
+    packageOverrides = pkgs: rec {
 
-  gsasl = pkgs.stdenv.lib.overrideDerivation pkgs.gsasl (x : {
-    configureFlags = "--with-gssapi-impl=mit";
-    nativeBuildInputs = x.nativeBuildInputs ++ [ pkgs.libkrb5 ];
-  });
+        st = pkgs.st.override {conf = (builtins.readFile ./st/config.def.h); };
 
-  st = pkgs.st.override { conf = (builtins.readFile ./st/config.def.h); };
+        surf = pkgs.surf.override { patches = ./surf.diff; };
 
- };
+        gsasl = pkgs.stdenv.lib.overrideDerivation pkgs.gsasl (oldAttrs : {
+                nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.krb5Full];
+                configureFlags = "--with-gssapi-impl=mit";
+        });
+
+        # Small core of things I need.
+        coreEnv = with pkgs; buildEnv {
+            name = "coreEnv";
+            paths = [
+                abduco
+                ack
+                curl
+                dmenu
+                dvtm
+                entr
+                ired
+                file
+                gist
+                git
+                moreutils
+                (neovim.override {vimAlias = true;})
+                netcat-openbsd
+                nix-repl
+                nq
+                pv
+                slmenu
+                st
+                stBig
+                vis
+                wget
+                which
+                xclip
+                xdotool
+                zathura
+            ];
+        };
+
+        # A more complete base system that I don't want to rebuild
+        # as frequently as core.
+        baseEnv = with pkgs; buildEnv {
+            name = "baseEnv";
+            paths = [
+                firefox
+                surf
+                tabbed
+            ];
+        };
+
+        # Stuff I need for work
+        workEnv = with pkgs; buildEnv {
+            name = "workEnv";
+            paths = [
+                cntlm
+            ];
+        };
+
+    };
 }
